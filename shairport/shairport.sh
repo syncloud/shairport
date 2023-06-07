37eabf7a5c94b603e64b90259ba4b885a556fc0f
Boris Rybalkin <ribalkin@gmail.com>
2023-06-07 20:40:09 +0100

    fix libs

--- shairport/shairport.sh
+++ shairport/shairport.sh
@@ -1,7 +1,12 @@
 #!/bin/bash -xe
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-LIBS=$(echo ${DIR}/lib/*-linux-gnu*)
-LIBS=$LIBS:$(echo ${DIR}/usr/lib/*-linux-gnu*)
 LIBS=$LIBS:$(echo ${DIR}/usr/lib)
+LIBS=$LIBS:$(echo ${DIR}/usr/lib/engines-3)
+LIBS=$LIBS:$(echo ${DIR}/usr/lib/libv4l)
+LIBS=$LIBS:$(echo ${DIR}/usr/lib/libv4l/plugins)
+LIBS=$LIBS:$(echo ${DIR}/usr/lib/ossl-modules)
+LIBS=$LIBS:$(echo ${DIR}/usr/lib/pulseaudio)
+LIBS=$LIBS:$(echo ${DIR}/usr/lib/vdpau)
 LIBS=$LIBS:$(echo ${DIR}/usr/local/lib)
-exec ${DIR}/lib/*-linux*/ld-*.so --library-path $LIBS ${DIR}/usr/local/bin/shairport-sync "$@"
+
+exec ${DIR}/lib/ld-musl-*.so* --library-path $LIBS ${DIR}/usr/local/bin/shairport-sync "$@"
--- test/upgrade.py
+++ test/upgrade.py
@@ -30,5 +30,4 @@ def test_upgrade(device, device_user, device_password, device_host, app_archive_
     device.run_ssh('snap remove shairport')
     #device.run_ssh('snap install shairport', retries=10)
     local_install(device_host, device_password, app_archive_path)
-    wait_for_rest(requests.session(), "https://{0}".format(app_domain), 200, 100)
 
