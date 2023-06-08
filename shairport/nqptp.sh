5f674a3a2c47a69250c7fe3420571622099cc126
Boris Rybalkin <ribalkin@gmail.com>
2023-06-08 09:54:21 +0100

    nqptp

---
+++ shairport/nqptp.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -xe
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+LIBS=$LIBS:$(echo ${DIR}/lib)
+LIBS=$LIBS:$(echo ${DIR}/usr/lib)
+LIBS=$LIBS:$(echo ${DIR}/usr/lib/engines-3)
+LIBS=$LIBS:$(echo ${DIR}/usr/lib/libv4l)
+LIBS=$LIBS:$(echo ${DIR}/usr/lib/libv4l/plugins)
+LIBS=$LIBS:$(echo ${DIR}/usr/lib/ossl-modules)
+LIBS=$LIBS:$(echo ${DIR}/usr/lib/pulseaudio)
+LIBS=$LIBS:$(echo ${DIR}/usr/lib/vdpau)
+LIBS=$LIBS:$(echo ${DIR}/usr/local/lib)
+
+exec ${DIR}/lib/ld-musl-*.so* --library-path $LIBS ${DIR}/usr/local/bin/nqptp "$@"
